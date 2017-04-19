8b575880907baed7c12dc4a2c7a05199ddc932a4
MikylaWilfred0306 <mikyla.wilfred@gmail.com>
2017-04-19 01:17:25 -0400

    After 4 different attempts of looking at this, it is now working

--- burst.c
+++ burst.c
@@ -17,6 +17,9 @@ Burst.c
 #include <pthread.h>
 #include <time.h>
 
+#define BLOCK 512
+
+
 struct threaddata_t {
   int id;
   int status;
@@ -45,36 +48,35 @@ void* process_thread(void* args) {
 
 
 
-#define BLOCK 512
-
-
 
 int countlines(char *filename)
 {
-  // count the number of lines in the file called filename                                    
-  FILE *fp = fopen(filename,"r");
-  int ch=0;
-  int lines=0;
 
-  if (fp == NULL);
-  return 0;
-
-  lines++;
-  while(!feof(fp))
-	{
-	  ch = fgetc(fp);
-	  if(ch == '\n')
-	  {
-		lines++;
-	  }
-	}
-  fclose(fp);
-  return lines;
+FILE *fp;
+    int count = 0;  // Line counter (result)
+	
+	 char c;  // To store a character read from file
+// Open the file
+    fp = fopen(filename, "r");
+ 
+    // Check if file exists
+    if (fp == NULL)
+    {
+        printf("Could not open file %s", filename);
+        return 0;
+    }
+ 
+    // Extract characters from file and store in character c
+    for (c = getc(fp); c != EOF; c = getc(fp))
+        if (c == '\n') // Increment count if this character is newline
+            count = count + 1;
+ 
+  return count;
 }
 
 
 int main(int argc, char* argv[]) {
-
+int lines = countlines(argv[1]);
 char const* const fileName = argv[1]; /* should check that argc > 1 */
 FILE* file = fopen(fileName, "r"); /* should check the result */
 char line[256];
@@ -84,15 +86,27 @@ char fileout1[256];
 strcpy(fileout1, argv[2]);
 
 
-int lines = countlines(argv[1]);
+
 int file_amount = lines / 500;
 int numthreads = file_amount + 1;
 int count = 1; 
 int outfd = STDOUT_FILENO;
 char buf[BLOCK];
 int num = 0;
-	for (int j = 0; j <= file_amount; j++){
 
+
+
+// allocate our the thread info
+struct threaddata_t* threadinfo = calloc(numthreads, sizeof(struct threaddata_t));
+if (!threadinfo) {
+	fprintf(stderr, "Unable to allocate thread info\n");
+	return 1;
+}
+	for (int j = 0; j < file_amount; j++){
+			threadinfo[j].id = j;
+
+			// use the thread to do some work
+			pthread_create(&threadinfo[j].tid, NULL, process_thread, &threadinfo[j]);
 
 			FILE *fptr;
 			strcpy(fileout,fileout1);
@@ -112,6 +126,8 @@ int num = 0;
 				fputs(line,fptr);
 				
 		}
+		
+		pthread_join(threadinfo[j].tid, NULL);    
 		num++;
 		
 	  }
@@ -129,15 +145,14 @@ int num = 0;
 		{
 			fptr = fopen(fileout, "wb");
 		}
-		int temp = num * 500;
+		int temp = file_amount * 500;
 		int temp2 = lines - temp;
-	  for (int i = 0; i <= 500; i++){
+		 fprintf(stdout, "BLAHHHH %d\n", temp2);
+	  for (int i = 1; i <= temp2; i++){
 				fgets(line, sizeof(line), file);
 				// Open file in write mode
 				fputs(line,fptr);
-				if (temp2 == i)
-					break;
-				
+							
 		}
 		
 		
